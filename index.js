592db883a9cb1308743abf3348643ad22d73816e
aiden <zero2one23581@gmail.com>
2022-03-09 10:16:24 +0900

    get

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import express from "express";
+
 const app = express();
 
-console.log("hello world");
+const PORT = 4000;
+const handleHome = (req, res) => {
+  return res.send("hello");
+};
+
+const firstmiddleware = (req, res, next) => {
+  console.log("first middleware");
+  next();
+};
+
+app.get("/", firstmiddleware, handleHome);
+
+app.listen(PORT, () => console.log(`✅ This server is on the port ${PORT} 🔥`));
