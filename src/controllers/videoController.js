e8159e35b71b8bd2fd9ebd8716829371dd8a5fe1
aiden <zero2one23581@gmail.com>
2022-03-16 19:39:11 +0900

    Post method practice

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,8 +30,21 @@ export const handleHome = (req, res) => {
   return res.render("video/home", { pageTitle: "Home", fakeUser, videos });
 };
 //CRUD
-export const createVideo = (req, res) => {
-  return res.render("video/create", { pageTitle: "Create Video", fakeUser });
+export const getUpload = (req, res) => {
+  return res.render("video/upload", { pageTitle: "Upload Video", fakeUser });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
 export const watchVideo = (req, res) => {
   const { id } = req.params;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {
-  createVideo,
+  getUpload,
   watchVideo,
   getEdit,
   postEdit,
   deleteVideo,
+  postUpload,
 } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.route("/create").get(createVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.route("/:id").get(watchVideo);
 videoRouter.route("/:id/edit").get(getEdit).post(postEdit);
 videoRouter.route("/:id/delete").get(deleteVideo);
