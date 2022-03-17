667df50587568e2ba5bb427d032e85eada9d9637
aiden <zero2one23581@gmail.com>
2022-03-17 21:17:54 +0900

    Search  video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("video/home", { pageTitle: "Home", videos });
   } catch {
     return res.send(`server-error`);
@@ -68,4 +68,18 @@ export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+//Search
+export const searchVideo = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("video/search", { pageTitle: `Search Video`, videos });
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,6 +6,7 @@ import {
   postEdit,
   deleteVideo,
   postUpload,
+  searchVideo,
 } from "../controllers/videoController";
 const videoRouter = express.Router();
 
@@ -13,5 +14,6 @@ videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.route("/:id([0-9a-f]{24})").get(watchVideo);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter.route("/search").get(searchVideo);
 
 export default videoRouter;
