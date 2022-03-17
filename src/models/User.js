016570db88e1277c677c9b040cf0c603734399fd
aiden <zero2one23581@gmail.com>
2022-03-17 23:54:12 +0900

    Create account hash pw

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { required: true, type: String, unique: true },
@@ -6,6 +7,12 @@ const userSchema = new mongoose.Schema({
   password: { required: true, type: String },
   name: { required: true, type: String },
   location: { type: String },
+});
+
+userSchema.pre("save", async function () {
+  console.log("USER PW:", this.password);
+  this.password = await bcrypt.hash(this.password, 5);
+  console.log("HASED PW:", this.password);
 });
 
 const User = mongoose.model("User", userSchema);
