03396f0eb6f88094dd8aa2d0bd838bf0a9110e21
aiden <zero2one23581@gmail.com>
2022-03-29 07:57:52 +0900

    ⚡️ Mongostore - session saved in mongodb!

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import MongoStore from "connect-mongo";
 import express from "express";
 import session from "express-session";
 import morgan from "morgan";
@@ -19,6 +20,9 @@ app.use(
     secret: "Hello",
     resave: true,
     saveUninitialized: true,
+    //아래 코드가 없으면 서버가 재시작할때 세션은 지워짐 (원래 세션은 메모리에 저장되기때문)
+    //connect-mongo를 설치하면 session이 mongodb에 저장되어서 서버가 꺼져도 데이터가 저장되어 브라우저가 항상 기억하게 됨!
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/juntube" }),
   })
 );
 
