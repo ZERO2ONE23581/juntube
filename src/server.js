e9d511aa864a8ac3b734db4c0dbf315d1e561c72
aiden <zero2one23581@gmail.com>
2022-03-29 08:07:02 +0900

    ⚡Unitialized Sessions

--- src/server.js
+++ src/server.js
@@ -18,8 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: "Hello",
-    resave: true,
-    saveUninitialized: true,
+    //아래 코드를 false로 바꾸면 모든 사람들의 웹사이트 방문을 저장하지는 않는다.
+    resave: false,
+    saveUninitialized: false,
     //아래 코드가 없으면 서버가 재시작할때 세션은 지워짐 (원래 세션은 메모리에 저장되기때문)
     //connect-mongo를 설치하면 session이 mongodb에 저장되어서 서버가 꺼져도 데이터가 저장되어 브라우저가 항상 기억하게 됨!
     store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/juntube" }),
